7a4fe28669e15e0e1cf76325ce309519f5ed3b54
S.Merki <samuel.merki@students.fhnw.ch>
2016-09-29 21:46:01 +0200

    inserted MainWindow

---
+++ source/WPF/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.Entity;
+using DotNetBay.Data.Provider.FileStorage;
+using DotNetBay.Interfaces;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string Reponame = "mainRepo";
+        public readonly IMainRepository MainRepository = new FileSystemMainRepository(Reponame);
+
+        public IAuctionRunner AuctionRunner { get; private set; }
+
+        public App()
+        {
+            this.AuctionRunner = new AuctionRunner(this.MainRepository);
+            this.AuctionRunner.Start();
+
+            var memberService = new SimpleMemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository, memberService);
+
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+        
+    }
+}
---
+++ source/WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DotNetBay.Core;
+using DotNetBay.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Auction> Auctions { get; private set; }
+        public AuctionService AuctionService { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            var app = (App)Application.Current;
+
+            AuctionService = new AuctionService(app.MainRepository, new SimpleMemberService(app.MainRepository));
+
+            Auctions = new ObservableCollection<Auction>(AuctionService.GetAll());
+
+        }
+
+    }
+}
